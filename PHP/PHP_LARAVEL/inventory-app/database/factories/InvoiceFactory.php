6d1701aded9d119f49f9a13f9bf7f4e097dc400f
DavidsDvm <davids10.dvm@gmail.com>
2021-09-21 11:49:42 -0500

    factorie of invoice

---
+++ PHP/PHP_LARAVEL/inventory-app/database/factories/InvoiceFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Invoice;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class InvoiceFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Invoice::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'subtotal' => random_int(100, 10000),
+            'total' => random_int(500, 50000)
+        ];
+    }
+}
