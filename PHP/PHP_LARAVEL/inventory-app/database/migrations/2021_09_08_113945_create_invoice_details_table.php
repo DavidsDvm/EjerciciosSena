d6da4f75c12e378a9f5fc2d74568ee9d3deea3ca
DavidsDvm <davids10.dvm@gmail.com>
2021-09-21 11:50:08 -0500

    migrations on the database about the invoice details

---
+++ PHP/PHP_LARAVEL/inventory-app/database/migrations/2021_09_08_113945_create_invoice_details_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoiceDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoice_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('invoice_id');
+            $table->foreignId('product_id');
+            $table->double('price');
+            $table->integer('quantity');
+            $table->timestamps();
+
+            $table->foreign('invoice_id')->references('id')->on('invoices');
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoice_details');
+    }
+}
